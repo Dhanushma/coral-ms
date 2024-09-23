04a26f186504e6e5e840c39b2498273235163551
dhanukd <dhnaushmadinesan@gmail.com>
2024-09-24 02:45:13 +0400

    code refactor

--- src/main/java/com/coral/dto/StatementRequestDTO.java
+++ src/main/java/com/coral/dto/StatementRequestDTO.java
@@ -2,9 +2,7 @@ package com.coral.dto;
 
 import com.coral.entity.StatementRequest;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -19,23 +17,27 @@ public class StatementRequestDTO {
     @Schema(
             description = "Account Number", example = "1234567890"
     )
-    @NotBlank(message = "Account number must not be empty")
+    @NotBlank(message = "Account number must not be blank")
     @Pattern(regexp = "\\d{10}", message = "Account number must be exactly 10 digits")
     private String accountNumber;
 
     @Schema(
             description = "Start date", example = "2024-08-20"
     )
-    @NotBlank(message = "From date must not be empty")
+    @NotBlank(message = "From date must not be blank")
     private String fromDate;
 
     @Schema(
             description = "End date", example = "2024-09-20"
     )
-    @NotBlank(message = "To date must not be empty")
+    @NotBlank(message = "To date must not be blank")
     private String toDate;
 
+    @Schema(
+            description = "Email ID", example = "name@gmail.com"
+    )
     @Email(message = "Please input a valid email Id")
+    @NotBlank(message = "Email Id must not be blank")
     private String emailId;
 
     public StatementRequest toStatementRequest() {
